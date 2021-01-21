ae2a8d94e9d8e12e72784e80c510d585ffaded4e
antoniorestivo <nonebesides370@gmail.com>
2021-01-20 17:23:50 -0700

    creates create route and action

--- app/controllers/api/cars_controller.rb
+++ app/controllers/api/cars_controller.rb
@@ -1,7 +1,15 @@
 class Api::CarsController < ApplicationController
   def index
-
-    render json: {message: "test"}
+    @cars = Car.all
+    render json: @cars
+  end
+  def create
+    @car = Car.new({
+      brand: params['brand'],
+      price: params['price']
+      })
+    @car.save
+    render json: @car
   end
   
 end
