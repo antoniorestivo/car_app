696ce743c914043c08618c0898643518db610dd8
antoniorestivo <nonebesides370@gmail.com>
2021-01-20 17:43:50 -0700

    creates remaining RESTful routes and controllers

--- app/controllers/api/cars_controller.rb
+++ app/controllers/api/cars_controller.rb
@@ -11,5 +11,23 @@ class Api::CarsController < ApplicationController
     @car.save
     render json: @car
   end
+  def show
+    @car = Car.find_by(id: params[:id])
+    render json: @car
+  end
+  def update
+    @car = Car.find_by(id: params[:id])
+    @car.price = params['price'] || @car.price
+    @car.brand = params['brand'] || @car.brand
+    @car.save
+    render json: @car
+  end
+  def destroy
+    @car = Car.find_by(id: params[:id])
+    @car.destroy
+    render json: {message: "Deleted car"}
+  end
+    
+  
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   namespace :api do
     get '/cars' => 'cars#index'
     post '/cars' => 'cars#create'
+    get '/cars/:id' => 'cars#show'
+    patch '/cars/:id' => 'cars#update'
+    delete '/cars/:id' => 'cars#destroy'
   end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
